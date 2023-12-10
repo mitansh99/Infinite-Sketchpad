2a1dffe0c014ee96016e7bfa93509444d4ba048f
mitansh99 <mitanshpatel99@gmail.com>
2023-12-11 00:35:11 +0530

    Mitansh

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+import '@tldraw/tldraw/tldraw.css'
+export default function () {
+	return (
+    <>
+   
+    <div style={{ position: 'fixed', inset: 0 }}>
+    
+			<Tldraw />
+		</div>
+    </>
+		
+	)
+}
