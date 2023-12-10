6ee7b7a070615d17fdcd6899e730a15e68efd217
Mitansh <95705819+mitansh99@users.noreply.github.com>
2023-12-11 00:44:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Tldraw } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 import '@tldraw/tldraw/tldraw.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function () {
 	return (
     <>
